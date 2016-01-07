b9817c82db654cf6e60473a792d4694c47b3ad07
StephenCWills <swills@gridprotectionalliance.org>
2016-01-07 00:13:13 -0500

    Updated WiX setup project to produce a working msi.

--- Source/Applications/PQDIFExplorer/Properties/AssemblyInfo.cs
+++ Source/Applications/PQDIFExplorer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PQDIFExplorer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Grid Protection Alliance")]
 [assembly: AssemblyProduct("PQDIFExplorer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.1.157.0")]
-[assembly: AssemblyVersion("2.1.157.0")]
-[assembly: AssemblyFileVersion("2.1.157.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("2.1.157-beta")]
